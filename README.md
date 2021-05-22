33e7f8bb9addbd0fd1df489351292f6e686a0828
aojie654 <aojie654@live.cn>
2021-05-22 15:05:55 +0800

    update README.md

---
+++ README.md
@@ -0,0 +1,119 @@
+# Jinmu cip (Check IP)
+
+## 0. 说明
+
+通常情况下的文字是描述, 在方框中的文字是终端中运行的命令. 不知道终端是啥? Google 一下?
+
+``` zsh
+# 框框里面的都是命令
+# 不过 # 开头的都是注释, 可以不用复制, 复制了也没用
+```
+
+## 1. cip (Shell)
+
+### 1.0 依赖第三方命令
+
+cip 中使用了以下第三方命令:
+
+- dig
+- curl
+
+### 1.1 Unix (Linux, MacOS)
+
+1. 放置在 __存在于系统环境变量的__ 的目录中, 例如:
+    - /usr/local/bin/
+    - /usr/bin/
+    - ~/.bin/ (推荐, 建议创建该目录后将其添加至 环境变量:PATH 中, 并将后续个人安装的其他命令行工具和自定义命令都放置于该文件夹中)
+2. 给 cip 文件通过 chmod 命令添加执行权限:
+
+    ``` zsh
+    chmod +x cip
+    ```
+
+3. cip用法如下:
+    - cip IP
+    - cip -f 包含ip列表的源文件 结果输出文件
+
+## 2. cip.py (Python)
+
+### 2.0 依赖第三方库
+
+- requests
+- dnspython
+
+### 2.1 Unix (Linux, MacOS)
+
+1. 将 cip.py 文件放置在一个 __自己能找得到__ 的目录中:
+    1. 通过 Jinmu GitLab:
+       1. 既然已经能看到这个 README 了, 那肯定是已经知道 git 怎么用了, git clone一下到本地吧
+    2. 通过 dls.jinmu.info 下载:
+       2. 下载链接: <https://dls.jinmu.info/iplocation/jm_akamai_cip/cip.py>
+2. 安装 Python3.8+ (官网 <https://www.python.org>, 安装方法自行搜索);
+3. pypi官方源速度慢的令人发指, 所以需要一个访问速度快的国内镜像源(可以理解成CDN).
+    通过以下命令, 为 Python PIP 配置 清华大学 镜像源:
+
+    ``` zsh
+    python3 -m pip install pip -U
+    python3 -m pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+    ```
+
+    参考链接: <https://mirrors.tuna.tsinghua.edu.cn/help/pypi/>
+4. 通过以下命令，为 Python 安装第三方库 requests, dnspython:
+
+    ``` zsh
+    python3 -m pip install requests dnspython
+    ```
+
+5. 在环境变量中添加如下行, 为 cip配置别名.
+   以我的环境为例, 使用了zsh, 环境变量在 ~/.zprofile中, cip文件的路径是 /Users/shengjyerao/git/jm_akamai_cip/cip.py:
+
+    ``` zsh
+    alias cip="python3 /Users/shengjyerao/git/jm_akamai_cip/cip.py"
+    ```
+
+6. 查看 cip 版本以检查配置是否正常:
+
+    ``` zsh
+    cip -v
+    ```
+
+7. cip Python版本用法: cip (-h, 可不输入, 缺省参数时默认输出帮助) 查看帮助
+
+### 2.2 Windows
+
+1. 参考 2.1 Unix(Linux, MacOS) 中的步骤 1-2 完成脚本下载和python安装;
+2. 在给 Windows Python pip 配置镜像源时，Python的命令不是 python3, 而是 python:
+
+    ``` zsh
+    python -m pip install pip -U
+    python -m pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+    ```
+
+3. 打开 PowerShell, 通过以下命令查看 PowerShell 配置文件路径:
+
+    ``` PowerShell
+    echo $PROFILE
+    ```
+
+    输出:
+
+    ``` PowerShell
+    C:\Users\shengjyerao\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
+    ```
+
+    这是输出只是指明 PowerShell 通过读取这个文件的内容来加载自定义的内容，并不代表这个文件一定存在，所以应该把自定义配置信息写在这个文件里面，当这个文件不存在的时候需要手动创建。
+
+4. 以文件路径 "C:\Users\shengjyerao\git\jm_akamai_cip\cip.py" 为例。
+   通过vscode打开上面获取到的 PowerShell 配置文件, 添加以下内容并保存:
+
+    ``` PowerShell
+    function cip {
+        python.exe C:\Users\shengjyerao\git\jm_akamai_cip\cip.py $args
+    }
+    ```
+
+5. 关闭并重新打开 PowerShell, 检查 cip 命令是否正常
+
+    ``` PowerShell
+    cip -v
+    ```
--- cip.py
+++ cip.py
@@ -6,7 +6,6 @@ cip(Check IP) 的 Python3 版本
 
 
 import argparse
-import json
 import os
 import re
 import sys
@@ -15,13 +14,34 @@ import dns.resolver
 import requests
 
 # cip 版本信息
-cip_info_version = {
-    "version": "0.1.0",
-    "timestamp": "2021-05-17 09:14:05",
-    "author_name": "aojie654",
-    "author_mail": "shengjie.ao@jinmuinfo.com"
-}
-__version__ = cip_info_version["version"]
+__version__ = "0.1.1"
+cip_info_version = """
+版本: {0}
+时间: 2021-05-18 13:17:58"
+编辑: aojie654"
+邮箱: shengjie.ao@jinmuinfo.com
+""".format(__version__)
+
+# 版本日志
+log_version = """
+v0.1.1:
+简介:
+- 无
+功能更新:
+- 增加了更新日志
+- 为版本更新时的输出增加换行, 并输出更新日志
+- 更新了版本信息显示方式
+- 更新了版本号 ╮(￣▽￣)╭
+
+v0.1.0:
+简介:
+- cip Python 版的第一个版本
+功能更新:
+- 支持多 IP/域名/文件 输入
+- 支持 非本地DNS
+- 增加了较为友好的帮助功能
+- 增加了较为便捷的更新功能
+"""
 
 # 定义cip服务器信息, DNS
 server_cip = "cip.jinmu.info"
@@ -162,12 +182,12 @@ def cip_domain(domain_tmp):
     # 初始化域名对应IP查询结果列表
     cip_list_domain = list()
     # 初始化域名查询开始和结束信息
-    domain_info = "{1} {0} {1}".format(domain_tmp, "=" * 30)
+    domain_info = "{0:=^54}".format(domain_tmp)
     # 遍历ip列表, 调用cip_ip, 并将结果追加至结果列表
     cip_list_domain.append(domain_info)
     for ip_tmp in ip_list:
         result_cip_ip = cip_ip(ip_tmp=ip_tmp)
-        cip_list_domain.append("{1} {0}".format(result_cip_ip, "=" * 2))
+        cip_list_domain.append("{1}{0}".format(result_cip_ip, "=" * 2))
     cip_list_domain.append(domain_info)
 
     return cip_list_domain
@@ -217,7 +237,7 @@ def get_version():
     获取当前版本
     """
 
-    result_version = json.dumps(cip_info_version, indent=4, ensure_ascii=False)
+    result_version = cip_info_version
 
     return result_version
 
@@ -227,16 +247,16 @@ def check_update():
     检查脚本更新
     """
     # 初始化 cip更新服务器 及 url
-    server_update = "dd.jinmu.info"
-    url_cip_file = "https://{0}/iplocation/cip.py".format(server_update)
-    url_cip_version = "https://{0}/iplocation/cip.version".format(server_update)
+    server_update = "dls.jinmu.info"
+    url_cip_file = "https://{0}/iplocation/jm_akamai_cip/cip.py".format(server_update)
+    url_cip_version = "https://{0}/iplocation/jm_akamai_cip/cip.version".format(server_update)
     # 创建对象获取远端版本
     result_request_cip_version = requests.get(url_cip_version)
     # 当状态码存在且为200时:
     if ((result_request_cip_version.status_code is not None) and (result_request_cip_version.status_code == 200)):
         # 获取版本信息内容并去除末尾回车, 输出更新信息
         result_remote = result_request_cip_version.text.rstrip()
-        update_info = "cip 更新 URL 为: {0}, 文件路径为: {1}, 当前版本为: {2}, cip 远端版本为: {3}, ".format(url_cip_file, __file__, __version__, result_remote)
+        update_info = "cip 更新 URL 为: {0}\n文件路径为: {1}\n当前版本为: {2}\ncip 远端版本为: {3}, ".format(url_cip_file, __file__, __version__, result_remote)
         print(update_info)
         if result_remote > __version__:
             # 当远端版本高于当前版本时, 进行文件更新
@@ -251,20 +271,32 @@ def check_update():
                 object_cip_file.write(result_request_cip_file.text)
                 object_cip_file.flush()
                 object_cip_file.close()
+                # 输出更新日志
+                log_version = get_log_version()
+                print(log_version)
                 # 输出更新完成状态
-                print("更新完毕!")
+                print("更新完成啦, 看看写代码的这次又新增了多少个bug *~(￣▽￣)~*")
             elif (result_request_cip_file.status_code is not None):
                 print("请求远端cip文件出错! 未收到请求响应码")
             else:
                 print("请求远端cip文件出错! 响应码: {0}".format(result_request_cip_file.status_code))
+        elif result_remote == __version__:
+            # 版本一致时提示无需更新
+            print("和远端版本一样咯, 没必要更新啦 *~(￣▽￣)~*")
         else:
-            # 否则提示无需更新
-            print("无需更新")
+            # 远端版本低于当前版本时
+            print("怕不是写代码的又偷懒了, 请喊他更新远端版本 *~(￣▽￣)~*")
     elif (result_request_cip_version.status_code is None):
-        print("获取远端版本出错! 未收到请求响应码")
+        print("获取远端版本出错! 未收到请求响应码!")
     else:
         print("获取远端版本出错! 状态码为: {0}".format(result_request_cip_version.status_code))
 
+def get_log_version():
+    """
+    返回 版本日志
+    """
+
+    return log_version
 
 if __name__ == "__main__":
     # 创建 参数解析对象
@@ -279,8 +311,9 @@ if __name__ == "__main__":
     parser.add_argument("-d", "--dns", nargs="+", help="在输入域名时指定 DNS服务器, 未指定时默认使用 Local DNS")
     # parser.add_argument("-h", "--help", help="显示该帮助文本后退出", dest="", action="store_true")
     parser.add_argument("-f", "--file", help="以文件作为输入, 并将结果追加至文件内", action="store_true")
+    parser.add_argument("-l", "--log", help="查看更新日志", action="store_true")
     parser.add_argument("-u", "--update", help="更新cip", action="store_true")
-    parser.add_argument("-v", "--version", help="显示当前文件版本及更新 URL", action="store_true")
+    parser.add_argument("-v", "--version", help="查看当前文件版本信息", action="store_true")
     # 解析参数
 
     # sys.argv = ("1.1.1.1 www.baidu.com 1.11.1.2 x")
@@ -293,22 +326,31 @@ if __name__ == "__main__":
     if len(sys.argv) == 1:
         # 当传入参数长度为1, 即未指定任何参数时, 默认输出帮助
         parser.print_help()
+    elif ("-l" or "--log") in sys.argv:
+        # 当参数中包含 -u 或者 --update 的时候检查更新
+        print(get_log_version())
     elif ("-u" or "--update") in sys.argv:
         # 当参数中包含 -u 或者 --update 的时候检查更新
         check_update()
-    elif ("-v" or "--version") in sys.argv:
+    elif ("-v") in sys.argv:
         # 当参数中包含 -v 或者 --version 的时候检查更新
         print(get_version())
+    elif("--version") in sys.argv:
+        # 只输出版本信息
+        print(__version__)
     else:
+        # 显示运行开始提示
+        print("{0:=^50}".format(" cip运行开始 "))
         args = parser.parse_args()
 
         # 使用输入时指定的dns服务器
         if args.dns:
             # 当DNS服务器不为空时, 使用传入的DNS服务器
             server_dns = args.dns
-            print("{1} 当前使用的DNS服务器为: {0} {1}".format(server_dns, "=" * 10))
+            info_dns = " 当前使用的DNS服务器为: {0} ".format(server_dns)
+            print("{0:=^45}".format(info_dns))
         else:
-            print("{0} 使用本地DNS服务器 {0}".format("=" * 10))
+            print("{0:=^47}".format(" 使用本地DNS服务器 "))
 
         # 判断输入的类型是否为文件
         if not args.file:
@@ -317,4 +359,5 @@ if __name__ == "__main__":
         else:
             # 存在 -f 时, 将 文件列表 作为输入调用 cip_file
             cip_file(file_list_tmp=args.input)
-    print("{0} cip 运行结束 {0}".format("=" * 10))
+        # 显示运行结束提示
+        print("{0:=^50}".format(" cip运行结束 "))
